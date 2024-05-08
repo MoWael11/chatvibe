f2a1cdbcc1394e7122dd6b91ca00faf85d65e140
Moahmed Wael <midonit06@gmail.com>
2024-05-08 19:00:23 +0200

    refactor: improved SEO

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -5,7 +5,15 @@ import { redirect } from 'next/navigation'
 const SignInPage = async ({ searchParams: { callbackUrl } }: { searchParams: { callbackUrl?: string } }) => {
   const session = await auth()
   if (session) redirect('/')
-  return <SignInModal callbackUrl={callbackUrl} />
+  return (
+    <>
+      <h1 className='sr-only'>ChatVibe | Connect and Chat with Friends</h1>
+      <h3 className='sr-only'>
+        A messaging application that allows you to connect and chat with friends in real-time. Join the conversation!
+      </h3>
+      <SignInModal callbackUrl={callbackUrl} />
+    </>
+  )
 }
 
 export default SignInPage
