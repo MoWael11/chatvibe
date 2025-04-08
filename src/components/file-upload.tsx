ebd2482e4a86150e1ee7c95a4fff30739adc77a2
MoWael11 <midonit06@gmail.com>
2025-04-09 00:03:49 +0200

    fix: fixed ui issues

--- src/components/file-upload.tsx
+++ src/components/file-upload.tsx
@@ -1,7 +1,8 @@
 import { useUploadThing } from '@/lib/uploadthing';
-import { Cloud, File } from 'lucide-react';
+import { Cloud, File, FileIcon, X } from 'lucide-react';
 import { useState } from 'react';
 import Dropzone from 'react-dropzone';
+import Image from 'next/image';
 import { Progress } from './ui/progress';
 import { useToast } from './ui/use-toast';
 
@@ -14,6 +15,8 @@ export const FileUpload = ({
   onChange: (url?: string) => void;
   value?: string;
 }) => {
+  const fileType = value?.split('.').pop();
+
   const [isUploading, setIsUploading] = useState<boolean>(false);
   const [uploadProgress, setUploadProgress] = useState<number>(0);
   const { toast } = useToast();
@@ -34,6 +37,44 @@ export const FileUpload = ({
 
     return interval;
   };
+
+  if (value && endpoint === 'serverImage' && fileType !== 'pdf') {
+    return (
+      <div className="relative h-20 w-20 m-auto">
+        <Image fill src={value} alt="Upload" className="rounded-full" />
+        <button
+          onClick={() => onChange('')}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  if (value && endpoint === 'serverImage' && fileType === 'pdf') {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10 m-auto">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value.split('/').pop()}
+        </a>
+        <button
+          onClick={() => onChange('')}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
 
   return (
     <Dropzone
